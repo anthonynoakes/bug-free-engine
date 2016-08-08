af2cc41f6a390a055938a38e71b8c167438e000d
Anthony Noakes <asn.noakes@gmail.com>
2016-08-08 00:24:42 -0700

    (attempting to learn git) rest of wirefram e for data flow

--- debug_tools/README.md
+++ debug_tools/README.md
@@ -1,1 +1,4 @@
 Dependancies, install NODE.js and NPM
+
+Shared enviorment setup with Angular
+https://docs.angularjs.org/tutorial/#environment-setup
--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -9,8 +9,52 @@ angular.
       var self = this;
       self.orderProp = 'age';
 
-      $http.get('phones/phones.json').then(function(response) {
-        self.phones = response.data;
+      var promises = []
+	  
+	  promises.push($http.get('players/players.json').then(function(response) {
+        self.players = response.data;
+      }));
+     
+      promises.push($http.get('medals/medals.json').then(function(response) {
+        self.medals = response.data;
+      }));
+
+      Promise.all(promises).then(values => {  
+		// Fill in meta data
+		self.players.forEach(player => {
+			// search medals for each 
+			fillPlayerMedalData(player, self.medals.medals);
+			player.total = calculateTotal(player)
+		})
+		
+		
       });
-    }
-  });
+  }
+});
+
+function fillPlayerMedalData(player, medals) {
+	player.teams.forEach(team => {
+		var metaData = medals.find(medal => medal.name == team.name);
+		
+		if(metaData) {
+			team.total = team.tier == "C" ? metaData.total * 5 : metaData.total;
+			team.metaData = metaData;
+		} else {
+			var x = team;
+		}
+	})
+}
+
+function calculateTotal(player) {
+	var total = 0;
+	player.teams.forEach(team => total+=team.total)
+	return total;
+}
+
+
+// 1: Name
+// 2: total
+// 3: 4 A teams
+// 4: 3 B teams
+// 5: 1 c team
+  
